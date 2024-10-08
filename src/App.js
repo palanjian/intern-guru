a0536a122e3c494b834a231b55844d8ce9f0aab5
Peter Palanjian <ppalanji@usc.edu>
2024-10-07 19:53:52 -0700

    Added firebase login/signup/logout

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import { Login } from './components/login'
+import { Signup } from './components/signup'
 
 function App() {
   return (
     <div className="App">
-      <h1>Internguru</h1>    
+      <h1>Internguru</h1>
+      <Login />
+      <Signup />
     </div>
   );
 }
---
+++ src/components/login.js
@@ -0,0 +1,49 @@
+import { auth, googleProvider } from '../config/firebase'
+import { signInWithEmailAndPassword, signInWithPopup, signOut } from 'firebase/auth'
+import React, { useState } from 'react';
+
+export const Login = () => {
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+
+    const signIn = async () => {
+        try{
+            await signInWithEmailAndPassword(auth, email, password)
+            console.log(auth?.currentUser?.email)
+        }
+        catch(err){ console.error(err) }
+    }
+
+    const signInWithGoogle = async () => {
+        try{
+            await signInWithPopup(auth, googleProvider)
+            console.log('Signed in with Google');
+        }
+        catch(err) { console.log(err) }
+    }
+
+    const logOut = async () => {
+        try{
+            await signOut(auth)
+            console.log(auth?.currentUser?.email)
+        }
+        catch(err) { console.log(err) }       
+    }
+
+    return(
+        <div>
+            <input 
+                placeholder="Email"
+                onChange={(e) => setEmail(e.target.value)}
+            />
+            <input 
+                placeholder="Password"
+                type="password"
+                onChange={(e) => setPassword(e.target.value)}
+            />
+            <button onClick={signIn}>Sign in</button>
+            <button onClick={signInWithGoogle}>Sign in with Google</button>
+            <button onClick={logOut}>Sign out</button>
+        </div>
+    )
+}
